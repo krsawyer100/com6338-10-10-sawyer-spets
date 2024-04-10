5e520556eccbd4410504f504d16799f7e6053e0b
Kylee Sawyer <k.sawyer@ufl.edu>
2024-04-09 23:56:27 -0400

    changes to cat.js

--- cat.js
+++ cat.js
@@ -4,7 +4,7 @@ const catCardsContainer = querySelector(".main__cat-cards-container")
 page.onload = async e => {
     try {
         //Fetch API info
-        const res = await fetch(``)
+        const res = await fetch(`https://api.rescuegroups.org/v5?limit=24&filterRadius=postalcode&distance=miles&key=1xbUifbS`)
         //Transform to JSON
         const catData = await res.json()
         //Display cat cards
@@ -14,6 +14,7 @@ page.onload = async e => {
 
 const renderCats = ({
     //getting specific data from api for use
+
 }) => {
     //cat information & reset
     catCardsContainer.innerHTML = ``
